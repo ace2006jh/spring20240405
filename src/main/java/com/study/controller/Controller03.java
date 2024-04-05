03093c86ddec6a02540ef864f753cd189f274667
parkjh <ace2006jh@naver.com>
2024-04-05 17:11:27 +0900

    first commit

---
+++ src/main/java/com/study/controller/Controller03.java
@@ -0,0 +1,45 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main3")
+public class Controller03 {
+
+    @RequestMapping("sub1")
+    public void method1() {
+        System.out.println("Controller03.method1");
+    }
+
+    @RequestMapping("sub2")
+    public void meethod2() {
+        System.out.println("Controller03.method2");
+    }
+
+    // 경로 : /main3/sub3
+    // 파라미터 : ?name
+    // /main3.sub3?name
+    @RequestMapping(value = "sub3", params = "name")
+    public void method3() {
+        System.out.println("Controller03.method3");
+    }
+
+    @RequestMapping(value = "sub3", params = {"name", "age"})
+    public void method4() {
+        System.out.println("Controller03.method4");
+    }
+
+    // /main3/sub3?address&city
+    @RequestMapping(value = "sub3", params = {"address", "city"})
+    public void method5() {
+        System.out.println("Controller03.method5");
+    }
+
+
+    // /main3/sub3?country&city
+    @RequestMapping(value = "sub3", params = {"country", "city"})
+    public void method6() {
+        System.out.println("Controller03.method6");
+    }
+}
